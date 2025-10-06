41abb291a4fd6343ca897eceafe741ab6bf885bb
aleks2705 <aleksei.najmitdinov@gmail.com>
2025-10-06 11:23:19 +0200

    feat[models] BlockClass

---
+++ src/main/java/com/richmillionaire/richmillionaire/models/Block.java
@@ -0,0 +1,16 @@
+package com.richmillionaire.richmillionaire.models;
+
+public class Block {
+    private String hash;
+    private String previousHash;
+    private String data;
+    private long timeStamp;
+    private int nonce;
+ 
+    public Block(String data, String previousHash, long timeStamp) {
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = timeStamp;
+        this.hash = calculateBlockHash();
+    }
+}
