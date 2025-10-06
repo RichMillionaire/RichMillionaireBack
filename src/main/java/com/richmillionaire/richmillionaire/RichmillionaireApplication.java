eee0cd55698c6958264d787c29a1d9f0429199dd
hugobordier <hugo.bordier2003@gmail.com>
2025-10-06 11:11:58 +0200

    Initial commit: commit automatique de Tonton 🚀

---
+++ src/main/java/com/richmillionaire/richmillionaire/RichmillionaireApplication.java
@@ -0,0 +1,13 @@
+package com.richmillionaire.richmillionaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RichmillionaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RichmillionaireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/richmillionaire/richmillionaire/RichmillionaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richmillionaire.richmillionaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RichmillionaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
