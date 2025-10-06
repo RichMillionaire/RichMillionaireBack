9570de53d6583119788d74320fd49d7f4610eb08
hugobordier <hugo.bordier2003@gmail.com>
2025-10-06 17:14:43 +0200

    fix : add connexion to databse

--- src/test/java/com/richmillionaire/richmillionaire/RichmillionaireApplicationTests.java
+++ src/test/java/com/richmillionaire/richmillionaire/RichmillionaireApplicationTests.java
@@ -1,13 +1,12 @@
 package com.richmillionaire.richmillionaire;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RichmillionaireApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
