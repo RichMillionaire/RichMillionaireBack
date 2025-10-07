0986a1172020c15a0698e8f33af80bf1b5210923
hugobordier <hugo.bordier2003@gmail.com>
2025-10-07 13:36:42 +0200

    update: add new common folders

--- src/main/java/com/richmillionaire/richmillionaire/models/User.java
+++ src/main/java/com/richmillionaire/richmillionaire/models/User.java
@@ -1,6 +1,13 @@
 package com.richmillionaire.richmillionaire.models;
 
-import jakarta.persistence.*;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,8 +22,8 @@ import lombok.Setter;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
     @Column(nullable = false, unique = true)
     private String username;
