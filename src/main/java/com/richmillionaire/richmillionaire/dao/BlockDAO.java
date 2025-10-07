c8dbce4c6143004d788d1391df43cea02126140c
hugobordier <hugo.bordier2003@gmail.com>
2025-10-07 15:50:27 +0200

    fix : ignore application.properties

--- src/main/java/com/richmillionaire/richmillionaire/dao/BlockDAO.java
+++ src/main/java/com/richmillionaire/richmillionaire/dao/BlockDAO.java
@@ -5,8 +5,12 @@ import java.util.List;
 import com.richmillionaire.richmillionaire.models.Block;
 
 public interface BlockDAO {
+
     void save(Block block) throws Exception; //save block into chain
+
     List<Block> findAll() throws Exception; //retourne tous les blocks stockés
+
     Block findById(int id) throws Exception; // retourne un block précis
+    
     void deletebyId(int id) throws Exception;
 }
