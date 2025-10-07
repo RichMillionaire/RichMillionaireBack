06cef0281cff1bd83c9d5aa589b620344d8a9295
yann6182 <yannickngaleu6@gmail.com>
2025-10-07 15:33:57 +0200

    user issue

--- src/main/java/com/richmillionaire/richmillionaire/RichmillionaireApplication.java
+++ src/main/java/com/richmillionaire/richmillionaire/RichmillionaireApplication.java
@@ -2,8 +2,12 @@ package com.richmillionaire.richmillionaire;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.richmillionaire.richmillionaire.dao")
+@EntityScan(basePackages = "com.richmillionaire.richmillionaire.entity")
 public class RichmillionaireApplication {
 
 	public static void main(String[] args) {
