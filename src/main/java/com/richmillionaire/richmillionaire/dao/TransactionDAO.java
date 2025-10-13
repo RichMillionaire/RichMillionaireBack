a9410567e4339b2aa5dabdfaa2ccf4d144328d28
aleks2705 <aleksei.najmitdinov@gmail.com>
2025-10-13 10:53:47 +0200

    feat[transactionDAO] ok

---
+++ src/main/java/com/richmillionaire/richmillionaire/dao/TransactionDAO.java
@@ -0,0 +1,15 @@
+package com.richmillionaire.richmillionaire.dao;
+
+import java.util.List;
+
+import com.richmillionaire.richmillionaire.models.Transaction;
+
+public interface TransactionDAO {
+    void save(Transaction transaction) throws Exception;
+
+    List<Transaction> findAll() throws Exception;
+
+    Transaction findById(String id) throws Exception;
+
+    void deleteById(String id) throws Exception;
+}
